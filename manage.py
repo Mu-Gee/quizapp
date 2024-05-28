6eecd1343d34fb99d9a6e29d12aed8511f1d6be3
Mu-Gee <61751306+Mu-Gee@users.noreply.github.com>
2024-05-28 16:54:30 +0300

    reverting

--- manage.py
+++ manage.py
@@ -2,14 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-#from django.core.management import execute_from_command_line
-from django.contrib.auth.models import User
 
-def create_superuser():
-    """Create superuser if it doesn't exist."""
-    if not User.objects.filter(is_superuser=True).exists():
-        User.objects.create_superuser('admin', 'admin@quizzapp.com', '@dm!n1quiz')
-        print('Superuser created successfully')
 
 def main():
     """Run administrative tasks."""
@@ -22,9 +15,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
-
-    create_superuser()  # Create superuser if it doesn't exist
     execute_from_command_line(sys.argv)
 
 
