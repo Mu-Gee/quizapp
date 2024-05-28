f4e6b77b6c666e0617b8f05a9e60ae1fe8ef6a8d
Mu-Gee <61751306+Mu-Gee@users.noreply.github.com>
2024-05-28 14:59:48 +0300

    configuring urls.static

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path("account/", include("account.urls")),
     path("quiz/", include("quiz.urls")),
     path("management/", include("management.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
